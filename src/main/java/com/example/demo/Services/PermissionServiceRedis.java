fcc8f5090709f194cee3e0478fb6cb2cb0e193c8
swaraj39 <swarajgujar022@gmail.com>
2026-04-17 11:41:23 +0530

    1

---
+++ src/main/java/com/example/demo/Services/PermissionServiceRedis.java
@@ -0,0 +1,24 @@
+package com.example.demo.Services;
+
+import com.example.demo.Models.ApiPermissions;
+import com.example.demo.Repository.APIPermissionsRepo;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PermissionServiceRedis {
+
+    private final APIPermissionsRepo repo;
+
+    public PermissionServiceRedis(APIPermissionsRepo repo) {
+        this.repo = repo;
+    }
+
+    @Cacheable(value = "permissions", key = "#role")
+    public List<ApiPermissions> getPermissionsByRole(String role) {
+        System.out.println("🔥 DB HIT FOR PERMISSIONS 1st time");
+        return repo.findByRolename(role);
+    }
+}
